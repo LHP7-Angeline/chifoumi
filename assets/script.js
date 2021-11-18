1d0d18335b2f8d00bb5bc89413a52f0a05fa95b2
Angeline <angelinecoignet@gmail.com>
2021-11-18 09:07:53 +0100

    Inversion des écrans joueurs et suppression des lignes commentées js

--- assets/script.js
+++ assets/script.js
@@ -10,7 +10,7 @@ let myResult = document.getElementById('screenResult');
 let playerScore = 0;
 let IAScore = 0;
 
-//Affecter une arme au hasard à l'IA
+
 //Retourne le nom d'une équipe en string
 function pickWeapon(list) {
     return list[Math.floor(Math.random() * list.length)];
@@ -30,21 +30,6 @@ function launchNewPlay() {
 document.getElementById('newPlay').onclick = () => launchNewPlay();
 
 
-//Choisir une arme
-// function chooseWeapon(element) {
-
-// if (element.target.nodeName == 'BUTTON' && element.target.id != 'newPlay') {
-// IA = pickWeapon(weapon);
-// player = element.target.value;
-// document.getElementById('screenWeaponPlayer').textContent = element.target.textContent;
-// document.getElementById('screenWeaponIA').textContent = IA.charAt(0).toUpperCase() + IA.slice(1);
-
-//         myResult.innerHTML = displayResult(); 
-//     }
-
-// }
-
-// document.addEventListener('click', chooseWeapon);
 
 //Choisir une équipe
 function chooseTeam(element) {
@@ -62,23 +47,6 @@ function chooseTeam(element) {
 document.addEventListener('click', chooseTeam);
 
 
-//Afficher le résultat de la manche
-// function displayResult() {
-//     if (player === IA) {
-//         return 'Egalité';
-//     // } else if ((player === 'pierre' && IA === 'feuille') || (player === 'feuille' && IA === 'ciseaux') || (player === 'ciseaux' && IA === 'pierre')) {
-//     } else if ((player === 'pierre' && IA === 'feuille') || (player === 'feuille' && IA === 'ciseaux') || (player === 'ciseaux' && IA === 'pierre')) { 
-//         IAScore++;
-//         document.getElementById('scoreAI').textContent = IAScore;
-//         return 'Perdu';
-//     } else{
-//         playerScore++;
-//         document.getElementById('scoreUser').textContent = playerScore;
-//         return 'Gagné'; 
-//     }
-// }
-
-
 function displayResult() {
     if (player === IA) {
         return 'Egalité';
@@ -92,3 +60,6 @@ function displayResult() {
         return 'Gagné';
     }
 }
+
+
+
